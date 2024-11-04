5f85ed51540646a547a60f371ad4b11e09ecb630
chenxuan <1607772321@qq.com>
2024-11-04 22:49:49 +0800

    feat:update release.sh

--- src/release.sh
+++ src/release.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 rm ./cppnet.tar.gz
+rm ./cppnet-ssl.tar.gz
 rm ./lib/libcppnet.a
-
-# run build.sh first
-./build.sh
 
 # copy src and lib to release dir
 release_dir=./release
@@ -17,11 +15,23 @@ mkdir -p $release_dir
 mkdir -p $include_dir
 mkdir -p $lib_dir
 cp -r ./cppnet $include_dir
-cp ./lib/libcppnet.a $lib_dir
 cp ../LICENSE $cppnet_dir
 
 # del all cpp file
 find $include_dir -name "*.cpp" -delete
+
+# run build.sh first
+./build.sh ON Release
+cp ./lib/libcppnet.a $lib_dir
+
+# tar release dir
+cd $release_dir
+tar -zcvf ../cppnet-ssl.tar.gz .
+cd ..
+
+# build with out ssl
+./build.sh OFF Release
+cp ./lib/libcppnet.a $lib_dir
 
 # tar release dir
 cd $release_dir
@@ -30,3 +40,4 @@ cd ..
 
 # del release dir
 rm -rf $release_dir
+
