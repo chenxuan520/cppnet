9e96bc8455d4d920e3c9dbb321ad2a8eed9559f3
chenxuan <1607772321@qq.com>
2024-11-12 21:27:05 +0800

    feat: format code

--- src/cppnet/server/io_multiplexing/select.cpp
+++ src/cppnet/server/io_multiplexing/select.cpp
@@ -1,13 +1,12 @@
 
 #ifdef _WIN32
 #include "select.hpp"
-#include <winsock2.h>
 #include <iostream>
+#include <winsock2.h>
 
 namespace cppnet {
 
-Select::Select() : IOMultiplexingBase() {
-}
+Select::Select() : IOMultiplexingBase() {}
 Select::~Select() {}
 
 int Select::Init() {
@@ -17,8 +16,8 @@ int Select::Init() {
 
 int Select::MonitorSoc(const Socket &fd) {
   FD_SET(fd.fd(), &monitor_set_);
-  if(fd.fd()>max_fd_){
-    max_fd_=fd.fd();
+  if (fd.fd() > max_fd_) {
+    max_fd_ = fd.fd();
   }
   return 0;
 }
@@ -34,23 +33,23 @@ int Select::Loop(NotifyCallBack callback) {
   int ret = 0;
   while (loop_flag_) {
     fd_set event_set = monitor_set_;
-    std::cout<<max_fd_+1<<" "<<event_set.fd_count<<std::endl;
+    std::cout << max_fd_ + 1 << " " << event_set.fd_count << std::endl;
     ret = select(0, &event_set, nullptr, nullptr, nullptr);
-    std::cout<<max_fd_+1<<ret<<std::endl;
+    std::cout << max_fd_ + 1 << ret << std::endl;
     if (ret < 0) {
       return ret;
     }
     for (int i = 0; i < monitor_set_.fd_count; ++i) {
       if (FD_ISSET(monitor_set_.fd_array[i], &event_set)) {
         Socket socket(monitor_set_.fd_array[i]);
-        std::string tmp_buf(1,0);
-        auto rc=socket.ReadPeek(tmp_buf, 1);
-        if(rc==0){
-          callback(*this, socket,kIOEventLeave);
-        }else if(rc>0){
-          callback(*this, socket,kIOEventRead);
-        }else{
-          callback(*this, socket,kIOEventError);
+        std::string tmp_buf(1, 0);
+        auto rc = socket.ReadPeek(tmp_buf, 1);
+        if (rc == 0) {
+          callback(*this, socket, kIOEventLeave);
+        } else if (rc > 0) {
+          callback(*this, socket, kIOEventRead);
+        } else {
+          callback(*this, socket, kIOEventError);
         }
         // callback(*this, monitor_set_.fd_array[i],kIOEventRead);
       }
