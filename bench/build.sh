2cb4f83136338b37c47e0b4a7f5d26c46744b194
chenxuan <1607772321@qq.com>
2024-11-25 21:06:36 +0800

    feat:add bench cicd

--- bench/build.sh
+++ bench/build.sh
@@ -2,16 +2,26 @@
 
 path=$(pwd)
 
+# build
 cd ../demo/simple-http-server
 ./build.sh
-../bin/demo >/dev/null &
-
 cd $path
-echo "GET http://127.0.0.1:8080/" | ./vegeta attack -name=200qps -rate=200 -duration=2s > result200.bin
-echo "GET http://127.0.0.1:8080/" | ./vegeta attack -name=400qps -rate=400 -duration=2s > result400.bin
-echo "GET http://127.0.0.1:8080/" | ./vegeta attack -name=800qps -rate=800 -duration=2s > result800.bin
-echo "GET http://127.0.0.1:8080/" | ./vegeta attack -name=1000qps -rate=1000 -duration=2s > result1000.bin
-./vegeta plot result200.bin result400.bin result800.bin result1000.bin > plot.html
 
-rm ./results.bin
-killall demo
+echo -e "\nBegin bench test"
+
+for ((i=500; i< 7000; i+=500)); do
+
+    cd ../demo/simple-http-server
+    # ./build.sh
+    ../bin/demo >/dev/null &
+    cd $path
+
+    echo "GET http://127.0.0.1:8080/" | ./vegeta attack -name=${i}qps  -rate=$i  -duration=2s > result${i}.bin
+    killall demo
+    cat result${i}.bin | ./vegeta report
+    ./vegeta report -type=json result${i}.bin > metrics${i}.json
+done
+./vegeta plot *.bin > plot.html
+
+
+rm *.bin
---
+++ bench/plot.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_point = np.array([])
+y_mean = np.array([])
+y_p99 = np.array([])
+y_right_rate = np.array([])
+arr = []
+
+i = 500
+while i < 7000:
+    # load metrics{i}.json file as json, append to arr
+    with open(f'metrics{i}.json', 'r') as f:
+        data = json.load(f)
+    arr.append(data)
+    x_point = np.append(x_point, i)
+    y_mean = np.append(y_mean, float(data['latencies']['mean'])/1e6)
+    y_p99 = np.append(y_p99, float(data['latencies']['99th'])/1e6)
+    y_right_rate = np.append(y_right_rate, float(data['status_codes']['200'])/float(data['requests']))
+
+    i += 500
+
+# draw and save to mean.png
+fig, axs = plt.subplots(1, 2)
+axs[0].plot(x_point, y_mean, label='mean')
+axs[0].plot(x_point, y_p99, label='p99')
+axs[0].legend()
+axs[0].set_xlabel('QPS')
+axs[0].set_ylabel('Latency(ms)')
+axs[0].set_title('Latency vs QPS')
+
+# draw right rate
+axs[1].plot(x_point, y_right_rate, label='right rate')
+axs[1].legend()
+axs[1].set_xlabel('QPS')
+axs[1].set_ylabel('Right Rate')
+axs[1].set_title('Right Rate vs QPS')
+
+fig.tight_layout()
+plt.savefig('result.png')
+plt.show()
