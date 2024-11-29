06be619a5d11c31e3a0c9f25896326ad8eda5d4f
chenxuan <1607772321@qq.com>
2024-11-29 21:20:58 +0800

    fix:demo problem

--- demo/use-lib-server/init.sh
+++ demo/use-lib-server/init.sh
@@ -13,7 +13,7 @@ if [ ! -d "./cppnet" ]; then
         echo "未知系统"
         exit
     fi
-    file_name=cppnet-$sysname-latest-ssl-release.tar.gz
+    file_name=cppnet-$sysname-latest-release.tar.gz
     wget https://github.com/chenxuan520/cppnet/releases/latest/download/$file_name
     tar -zxvf $file_name
     rm $file_name
