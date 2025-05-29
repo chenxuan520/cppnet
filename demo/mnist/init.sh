f108ad049ab3637ae25eaa065e5618ad36e92c09
chenxuan <1607772321@qq.com>
2025-05-29 11:33:40 +0800

    refactor: replace git submodule with direct clone and handle GitHub CICD environment

--- demo/mnist/init.sh
+++ demo/mnist/init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-git clone https://gitee.com/chenxuan520/cppjson
-git clone https://gitee.com/chenxuan520/deeplearning.git
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    echo "In GitHub CICD environment"
+    git clone https://github.com/chenxuan520/cppjson
+    git clone https://github.com/chenxuan520/deeplearning.git
+else
+    echo "Not in GitHub CICD environment"
+    git clone https://gitee.com/chenxuan520/cppjson
+    git clone https://gitee.com/chenxuan520/deeplearning.git
+fi
 
 # if not exist demo.param
 if [ ! -f "./demo.param" ]; then
--- demo/web-server/init.sh
+++ demo/web-server/init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-git clone https://gitee.com/chenxuan520/cppjson
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    echo "In GitHub CICD environment"
+    git clone https://github.com/chenxuan520/cppjson
+else
+    echo "Not in GitHub CICD environment"
+    git clone https://gitee.com/chenxuan520/cppjson
+fi
 
 # if not exist ssl dir
 if [ ! -d "./ssl" ]; then
